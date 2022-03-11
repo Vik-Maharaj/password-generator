26310d5b2558e0fdd70d8250c294a14251982ed0
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-03-11 13:11:41 -0500

    Add random number logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,46 @@
-// this prompts the user when they first clicks the button
+// declared variables
 
 var generateBtn = document.querySelector("#generate");
+var userCharLength; 
+var userLowerCaseOrNo;
+var userUpperCaseOrNo;
+var userNumericCharOrNo;
+var userSpecCharOrNo;
+
+
+
+// random generator logic
+
+function vikRandomInt() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < userCharLength; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+
+
+
+
+// this prompts the user when they first clicks the button
 
 var startVPasswordGenerator = function() {
-  var userCharLength = window.prompt('Welcome. How many characters would you like your password to be? (choose a number between 8 and 128 characters)');
+  userCharLength = window.prompt('Welcome. How many characters would you like your password to be? (choose a number between 8 and 128 characters)');
   console.log(userCharLength);
 
-  var userLowerCaseOrNo = window.prompt('Would you like your password to include lowercase characters?');
+  userLowerCaseOrNo = window.prompt('Would you like your password to include lowercase characters?');
   console.log(userLowerCaseOrNo);
 
-  var userUpperCaseOrNo = window.prompt('Would you like your password to include uppercase characters?');
+  userUpperCaseOrNo = window.prompt('Would you like your password to include uppercase characters?');
   console.log(userUpperCaseOrNo);
 
-  var userNumericCharOrNo = window.prompt('Would you like your password to include numeric characters?');
+  userNumericCharOrNo = window.prompt('Would you like your password to include numeric characters?');
   console.log(userNumericCharOrNo);
 
-  var userSpecCharOrNo = window.prompt('Would you like your password to include special characters?');
+  userSpecCharOrNo = window.prompt('Would you like your password to include special characters?');
   console.log(userSpecCharOrNo);
 
 }
@@ -29,11 +54,10 @@ generateBtn.addEventListener("click", startVPasswordGenerator);
 
 
 
-
 // this returns a value to the text field after running through the prompts
 
 function writePasswordToField() {
-    var password = "hello again";
+    var password = vikRandomInt();
     var passwordText = document.querySelector("#password");
   
     passwordText.value = password;
